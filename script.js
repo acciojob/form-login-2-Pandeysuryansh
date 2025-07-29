0f4ce1026c184d4b81bbed97a0cb8ccba75e3f74
Pandeysuryansh <167824825+Pandeysuryansh@users.noreply.github.com>
2025-07-30 00:23:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.getElementById("userForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const firstName = document.querySelector('input[name="First Name"]').value;
+  const lastName = document.querySelector('input[name="Last Name"]').value;
+  const phone = document.querySelector('input[name="Phone Number"]').value;
+  const email = document.querySelector('input[name="Email ID"]').value;
+
+  alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`);
+});
